a926c41acbb2d8060e6614ca9f84419776bbc683
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-03-04 19:07:23 +0300

    Выполнила задание по уроку "CSS Modules"

--- src/components/application/application.jsx
+++ src/components/application/application.jsx
@@ -1,7 +1,7 @@
 import { InputItem } from '../input-item/input-item';
 import { ItemList } from '../item-list/item-list';
 import { Footer } from '../footer/footer';
-import './application.css';
+import styles from './application.module.css';
 import '../../assets/fonts.css';
 
 const Application = () => {
@@ -18,8 +18,8 @@ const Application = () => {
     ];
 
     return (
-        <div className="todo-list">
-            <h1 className="todo-list__title">todos</h1>
+        <div className={styles.wrap}>
+            <h1 className={styles.title}>todos</h1>
             <InputItem />
             <ItemList items={items} />
             <Footer count={3} />
