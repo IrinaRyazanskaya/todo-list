d33216bd54c13766c25f30383d765f112989e6b3
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-03-09 15:04:51 +0300

    Выполнила задание по уроку "Компоненты-классы"

--- src/components/application/application.jsx
+++ src/components/application/application.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import { InputItem } from '../input-item/input-item';
 import { ItemList } from '../item-list/item-list';
 import { Divider } from '../divider/divider';
@@ -5,38 +6,40 @@ import { Footer } from '../footer/footer';
 import styles from './application.module.css';
 import '../../assets/fonts.css';
 
-const Application = () => {
-    const items = [
-        {
-            value: 'Сделать уборку',
-            isDone: true,
-            optionId: 'thing-1'
-        },
-        {
-            value: 'Сходить в магазин',
-            isDone: false,
-            optionId: 'thing-2'
-        },
-        {
-            value: 'Приготовить ужин',
-            isDone: false,
-            optionId: 'thing-3'
-        }
-    ];
-
-    return (
-        <div className={styles.screen}>
-            <div className={styles.wrap}>
-                <h1 className={styles.title}>todo list</h1>
-                <div className={styles.todo}>
-                    <InputItem />
-                    <ItemList items={items} />
+class Application extends Component {
+    render() {
+        const items = [
+            {
+                value: 'Сделать уборку',
+                isDone: true,
+                optionId: 'thing-1'
+            },
+            {
+                value: 'Сходить в магазин',
+                isDone: false,
+                optionId: 'thing-2'
+            },
+            {
+                value: 'Приготовить ужин',
+                isDone: false,
+                optionId: 'thing-3'
+            }
+        ];
+    
+        return (
+            <div className={styles.screen}>
+                <div className={styles.wrap}>
+                    <h1 className={styles.title}>todo list</h1>
+                    <div className={styles.todo}>
+                        <InputItem />
+                        <ItemList items={items} />
+                    </div>
+                    <Divider />
+                    <Footer count={3} />
                 </div>
-                <Divider />
-                <Footer count={3} />
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export { Application };
