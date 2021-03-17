38ef5809147f8fef8e2c2de9740cead85fa10305
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-03-17 18:49:22 +0300

    Добавила React hooks

--- src/components/application/application.jsx
+++ src/components/application/application.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { InputItem } from '../input-item/input-item';
 import { ItemList } from '../item-list/item-list';
 import { Divider } from '../divider/divider';
@@ -6,29 +6,39 @@ import { Footer } from '../footer/footer';
 import styles from './application.module.css';
 import '../../assets/fonts.css';
 
-class Application extends Component {
-    state = {
-        items: [
-            {
-                value: 'Сделать уборку',
-                isDone: true,
-                optionId: 1
-            },
-            // {
-            //     value: 'Сходить в магазин',
-            //     isDone: false,
-            //     optionId: 2
-            // },
-            // {
-            //     value: 'Приготовить ужин',
-            //     isDone: false,
-            //     optionId: 3
-            // }
-        ],
-    };
+const initialState = {
+    items: [
+        {
+            value: 'Сделать уборку',
+            isDone: true,
+            optionId: 1
+        },
+        {
+            value: 'Сходить в магазин',
+            isDone: false,
+            optionId: 2
+        },
+        {
+            value: 'Приготовить ужин',
+            isDone: false,
+            optionId: 3
+        }
+    ],
+};
 
-    onClickDone = id => {
-        const newItemList = this.state.items.map(item => {
+const Application = () => {
+    const [items, setItems] = useState(initialState.items);
+
+    useEffect(() => {
+        console.log('update');
+    });
+
+    useEffect(() => {
+        console.log('mount');
+    }, []);
+
+    const onClickDone = id => {
+        const newItemList = items.map(item => {
             const newItem = { ...item };
 
             if (item.optionId === id) {
@@ -38,47 +48,47 @@ class Application extends Component {
             return newItem;
         });
 
-        this.setState({ items: newItemList });
+        setItems(newItemList);
     };
 
-    onClickDelete = id => {
-        const newItemList = this.state.items.filter(item => {
+    const onClickDelete = id => {
+        const newItemList = items.filter(item => {
             return item.optionId !== id;
         });
 
-        this.setState({ items: newItemList });
+        setItems(newItemList);
     };
 
-    onClickAdd = value => this.setState(state => ({
-        items: [
-            ...state.items,
+    const onClickAdd = value => {
+        const newItems = [
+            ...items,
             {
                 value,
                 isDone: false,
-                optionId: state.items.length + 1
+                optionId: items.length + 1
             }
-        ]
-    }));
+        ];
 
-    render() {
-        return (
-            <div className={styles.screen}>
-                <div className={styles.wrap}>
-                    <h1 className={styles.title}>todo list</h1>
-                    <div className={styles.todo}>
-                        <InputItem onClickAdd={this.onClickAdd} />
-                        <ItemList 
-                            items={this.state.items}
-                            onClickDone={this.onClickDone}
-                            onClickDelete={this.onClickDelete}
-                        />
-                    </div>
-                    <Divider />
-                    <Footer count={this.state.items.length} />
+        setItems(newItems);
+    };
+
+    return (
+        <div className={styles.screen}>
+            <div className={styles.wrap}>
+                <h1 className={styles.title}>todo list</h1>
+                <div className={styles.todo}>
+                    <InputItem onClickAdd={onClickAdd} />
+                    <ItemList
+                        items={items}
+                        onClickDone={onClickDone}
+                        onClickDelete={onClickDelete}
+                    />
                 </div>
+                <Divider />
+                <Footer count={items.filter(item => !item.isDone).length} />
             </div>
-        );
-    }
-};
+        </div>
+    );
+}
 
 export { Application };
