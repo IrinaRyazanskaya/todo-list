ed4d47bf3dde489346fbd311c5ffb7c4fd0db4ba
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-03-19 16:37:30 +0300

    Создала страницы "Обо мне" и "Контакты"

---
+++ src/components/about/about.jsx
@@ -0,0 +1,13 @@
+import styles from './about.module.css';
+
+const About = () => {
+    return (
+        <article className={styles.wrap}>
+            <h1 className={styles.header}>
+                About
+            </h1>
+        </article>
+    );
+};
+
+export { About };
---
+++ src/components/contacts/contacts.jsx
@@ -0,0 +1,13 @@
+import styles from './contacts.module.css';
+
+const Contacts = () => {
+    return (
+        <article className={styles.wrap}>
+            <h1 className={styles.header}>
+                Contacts
+            </h1>
+        </article>
+    );
+};
+
+export { Contacts };
