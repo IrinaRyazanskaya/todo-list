9cdcb8cdfdaa82cb4ae1336d1bac82a9ade4c6c8
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-04-20 13:31:37 +0300

    Создала компонент с информацией о пользователе

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,10 +1,99 @@
+import iconPhoneSrc from './phone-icon.svg';
+import iconEmailSrc from './email-icon.svg';
+import iconGithubSrc from './github-icon.svg';
+import iconTelegramSrc from './telegram-icon.svg';
+import iconInstagramSrc from './instagram-icon.svg';
+import iconVkSrc from './vk-icon.svg';
+
 import styles from './contacts.module.css';
 
-const Contacts = () => {
+const Contacts = ({ userInfo }) => {
     return (
-        <article className={styles.wrap}>
-            <h1 className={styles.header}>Contacts</h1>
-        </article>
+        <div className={styles.about}>
+            <img
+                src={userInfo.avatar_url}
+                alt="Аватар пользователя"
+                className={styles.photo}
+            />
+            <div className={styles.infoWrap}>
+                <div className={styles.info}>
+                    <p className={styles.name}>{userInfo.name}</p>
+                    <p className={styles.text}>{userInfo.bio}</p>
+                    <div className={styles.contact}>
+                        <img
+                            className={styles.contactIcon}
+                            src={iconEmailSrc}
+                            alt="Иконка email"
+                        />
+                        <a
+                            className={styles.contactLink}
+                            href="mailto:ryazanskaya.irina@yandex.ru"
+                        >
+                            ryazanskaya.irina@yandex.ru
+                        </a>
+                    </div>
+                    <div className={styles.contact}>
+                        <img
+                            className={styles.contactIcon}
+                            src={iconPhoneSrc}
+                            alt="Иконка телефона"
+                        />
+                        <a
+                            className={styles.contactLink}
+                            href="tel:+79122790368"
+                        >
+                            +7 (912) 279-03-68
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.socialNetwork}>
+                    <a
+                        className={styles.socialLink}
+                        href={userInfo.html_url}
+                        target="_blank"
+                    >
+                        <img
+                            className={styles.socialIcon}
+                            src={iconGithubSrc}
+                            alt="Иконка GitHub"
+                        />
+                    </a>
+                    <a
+                        className={styles.socialLink}
+                        href="https://t.me/irina_ryazanskaya"
+                        target="_blank"
+                    >
+                        <img
+                            className={styles.socialIcon}
+                            src={iconTelegramSrc}
+                            alt="Иконка Telegram"
+                        />
+                    </a>
+                    <a
+                        className={styles.socialLink}
+                        href="https://www.instagram.com/irina__d/?hl=ru"
+                        target="_blank"
+                    >
+                        <img
+                            className={styles.socialIcon}
+                            src={iconInstagramSrc}
+                            alt="Иконка Instagram"
+                        />
+                    </a>
+                    <a
+                        className={styles.socialLink}
+                        href="https://vk.com/id52333127"
+                        target="_blank"
+                    >
+                        <img
+                            className={styles.socialIcon}
+                            src={iconVkSrc}
+                            alt="Иконка Вконтакте"
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 };
 
